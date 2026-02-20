63661f8ca03abbb363c701eb49ff7f349a441e2f
mauropasan <mauropasan01@gmail.com>
2026-02-20 15:14:02 +0100

    [ADD] Get Started

---
+++ get-started/myfirstprogram.cpp
@@ -0,0 +1,22 @@
+// iostream works with input/output objects such as cout
+#include <iostream>
+// Usage of objects and classes from the standard library
+using namespace std;
+
+// Main function
+int main() {
+  // cout displays output
+  cout << "Hello World!";
+  // Ends the main function
+  return 0;
+}
+
+
+/*
+We can use:
+- cout << "Hello World!";
+Another method we can use is to remove:
+- using namespace std;
+and use:
+- std::cout << "Hello World!";
+*/
