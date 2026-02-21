efc677351cda2dd0b6ef32ae30f90bb6f1520754
mauropasan <mauropasan01@gmail.com>
2026-02-21 18:07:34 +0100

    [ADD & MOD] Basic data types added + project restructure

---
+++ data-types/01-basic-data-types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 5;                // Integer (whole number
+    float floatNumber = 5.99;   // Float point number
+    double doubleNumber = 5.98; //Floating point number
+    char letter = 'A';          // Character
+    bool boolean = true;        // Boolean
+    string text = "Hello";      // String
+}
+
+/*
+  The main difference between float and double
+  is that float is 6/7 decimal digits, while
+  double is 15. Therefore double is safer to use
+  for most calculations.
+*/
