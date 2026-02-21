70a36ffb0adadc3ee512684aedcd20e04646fc18
mauropasan <mauropasan01@gmail.com>
2026-02-21 18:22:40 +0100

    [UPDATE] Added Basic Data Types comments

--- challenges/04-data-types-challenge.cpp
+++ challenges/04-data-types-challenge.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int studentID = 1;
-    float score = 10;
+    float score = 10.00;
     char grade = 'S';
     cout << studentID << endl;
     cout << score << endl;
--- data-types/01-basic-data-types.cpp
+++ data-types/01-basic-data-types.cpp
@@ -9,6 +9,7 @@ int main()
     char letter = 'A';          // Character
     bool boolean = true;        // Boolean
     string text = "Hello";      // String
+    auto x = 10;                // Determines the type through the assigned variable
 }
 
 /*
@@ -16,4 +17,35 @@ int main()
   is that float is 6/7 decimal digits, while
   double is 15. Therefore double is safer to use
   for most calculations.
+
+  int: allows you to use a whole number.
+  e. g. 10, 90, 1000, 5000...
+
+  float: uses decimal numbers up to 6 or 7 decimals.
+  E . g. 20.30, 10.90, 99.99...
+
+  double: same as float, but up to 15 decimals.
+
+  boolean: uses true or false (which means 1 or 0)
+
+  char: a single character can be used.
+  E. g. 'a', 'D', 'F'...
+
+  string: it's a sequence of characters, allowing
+  you to use multiple characters within.
+  E. g. "Hello", "World", "C++"...
+
+  auto: detects automatically the type that is assigned
+  through the assigned variable, meaning it can be any
+  of the previous mentioned.
+  E. g.
+
+  You can assign the value in the same line
+  auto x = 10; <- This will be assigned as an int
+
+  or
+
+  Declare the variable first and then detect it later on
+  auto x;               <- Only declares the variable
+  x = "Hello World";    <- x will be assigned as a string
 */
