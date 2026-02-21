51afb45b076e061bf9d53430f1bd4962af1fad03
mauropasan <mauropasan01@gmail.com>
2026-02-21 22:57:13 +0100

    [ADD] Math

---
+++ 10-math/01-math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath> // This library includes sqrt(), round(), log()... Maths calculations in short
+using namespace std;
+
+int main()
+{
+    cout << max(5, 10) << endl; // Will output the highest number
+    cout << min(5, 10) << endl; // Will output the lowest number
+    cout << sqrt(9) << endl;    // Outputs square root of 9 (3)
+    cout << round(2.6) << endl; // Will round the specified number (will show 3)
+    cout << log(2) << endl;
+    return 0;
+}
