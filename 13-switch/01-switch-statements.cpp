61b62cfc382c897732afe8e5281354c53279f97a
mauropasan <mauropasan01@gmail.com>
2026-02-22 13:41:06 +0100

    [ADD] Switch

---
+++ 13-switch/01-switch-statements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int expression = 1;
+
+    switch(expression) {
+      case 1:
+        // Execute if expression is 1;
+        break; // Specifies to get out of the switch
+      case 2:
+        // Execute if expression is 2;
+        break;
+      default:
+        // Execute if no condition above was made
+    }
+}
