1d4414a62879ced72926ca1fadd4ea486afb62bd
mauropasan <mauropasan01@gmail.com>
2026-02-22 14:08:24 +0100

    Fix comment on 02-do-while-loop.cpp

--- 14-while-loop/02-do-while-loop.cpp
+++ 14-while-loop/02-do-while-loop.cpp
@@ -18,4 +18,5 @@ int main()
   The difference between while and do while is that while
   will check first if the condition is made and then
   execute the code, do while does the opposite.
+  This means that do while will guaranteed to execute at least once
 */
